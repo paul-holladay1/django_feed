bf2da49969cc6c147887e2884585d5bcf9c1eec3
Paul <pholladay@test.com>
2022-12-11 15:45:42 -0600

    ready for heroku

--- FeedApp/admin.py
+++ FeedApp/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Profile,Comment,Like, Relationship
+from .models import *
 
 admin.site.register(Profile)
 admin.site.register(Comment)
